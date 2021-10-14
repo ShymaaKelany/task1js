78080a1d07fd088369393249cb3bc93085009834
Shymaa kelany <kelanymmd123@gmail.com>
2021-10-14 03:41:23 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+document .getElementById("result").onclick = function(){
+  var x = document.getElementById("x").value;
+  var y = document.getElementById("y").value;
+  var o = document.getElementById("o").value;
+   document.getElementById("out").innerHTML= +x + +y;
+
+}
